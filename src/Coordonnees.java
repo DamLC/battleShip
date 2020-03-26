46de7fb03181d37f444a484c878b467b4902fb21
Dam <dlc65@free.fr>
2020-03-26 15:03:03 +0100

    ajout class

---
+++ src/Coordonnees.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Coordonnees {
+
+    private static Random random;
+    private int x;
+    private int  y;
+
+    public Coordonnees(int x, int y) {
+        this.x = random.nextInt(10);
+        this.y =  random.nextInt(10);
+    }
+
+
+}
